5f0e70b79c0df549ac9f5e2cf97668a1cbb64e54
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-04-27 14:09:55 +0200

    feat : added Search

--- FrontEnd/src/router.js
+++ FrontEnd/src/router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Homepage from "./pages/Homepage/Homepage";
 import ProductInformation from "./pages/ProductInformation/ProductInformation";
+import Search from "./pages/Search/Search";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
         index: true,
         //element: <Homepage/>
         element: <ProductInformation />,
+      },
+      {
+        path: "search",
+        element: <Search />,
       },
     ],
   },
