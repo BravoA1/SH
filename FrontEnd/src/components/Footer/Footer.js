6eb5ac4771269c8e51347f7ee0f0181c1202dd65
Cyril-Dmrs <cyril.desmurs@outlook.com>
2023-05-22 09:49:04 +0200

    design

--- FrontEnd/src/components/Footer/Footer.js
+++ FrontEnd/src/components/Footer/Footer.js
@@ -1,12 +1,61 @@
 import styles from "./Footer.module.scss";
+import logo from "../../assets/images/logo_sockHub.png";
 
 export default function Footer() {
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
 
-
-  return (
-    <footer className={`${styles.footer}`}>
-      <p className="d-flex justify-content-center">Copyright © 2023 JWT Inc.</p>
-
-    </footer>
-  );
+	return (
+		<>
+			<div onClick={scrollToTop} className={`${styles.topPage}`}>
+				<p>Retour en haut</p>
+			</div>
+			<footer>
+				<div className={`${styles.logo}`}>
+					<img src={logo} alt="Logo" />
+				</div>
+				<div className={`${styles.col}`}>
+					<h1>A propos</h1>
+					<ul>
+						<li>Qui somme-nous ?</li>
+						<li>Question fréquente</li>
+						<li>Guide des tailles</li>
+					</ul>
+				</div>
+				<div className={`${styles.col}`}>
+					<h1>Mention Légale</h1>
+					<ul>
+						<li>Condition générales d'utilisation</li>
+						<li>Condition générales de vente</li>
+						<li>Condition générales de reprise</li>
+						<li>Protection des données</li>
+						<li>Cookies</li>
+						<li>Mention légale</li>
+					</ul>
+				</div>
+				<div className={`${styles.col}`}>
+					<h1>Social</h1>
+					<ul>
+						<li>Email : </li>
+						<li>Numéro : </li>
+						<div className={`${styles.social}`}>
+							<li>
+								<img src="https://svgshare.com/i/5eA.svg" width="32" />
+							</li>
+							<li>
+								<img src="https://svgshare.com/i/5eA.svg" width="32" />
+							</li>
+							<li>
+								<img src="https://svgshare.com/i/5f_.svg" width="32" />
+							</li>
+						</div>
+					</ul>
+				</div>
+			</footer>
+		</>
+	);
 }
